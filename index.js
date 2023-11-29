3ea706dc2b8d6c6b0a9fe41a6ce986e24468efd0
Hsu-hsuuuu <and.natarov@yandex.ru>
2023-11-29 16:39:48 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    slidesPerView: 4,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
